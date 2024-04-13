2033e5966c59cde88cebc9df55abd04b0c3b8034
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-04-13 10:25:13 -0300

    Criando Input's Personalizados

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,31 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:nail_app/template/components/input_personalizado.dart';
+import 'package:nail_app/template/components/subtitulo.dart';
+import 'package:nail_app/template/components/titulo.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Stack(
       children: [
-        Text("Open Nails"),
-        Text("Vamos à sua casa"),
-        Form(
+        Container(
+          width: MediaQuery.of(context).size.height,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/background.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Container(
+          // color: Colors.black.withOpacity(0.2), TODO: Remover isso quando pular pra próxima Página
           child: Column(
             children: [
-              TextField(
-                decoration: InputDecoration(
-                  label: Text("Email"),
+              Titulo(texto: "Open Nails"),
+              SubTitulo(texto: "Vamos à sua casa"),
+              Form(
+                child: Column(
+                  children: [
+                    InputPersonalizado(
+                      texto: "Email",
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 20),
+                      child: InputPersonalizado(
+                        texto: "Senha",
+                        senha: true,
+                      ),
+                    )
+                  ],
                 ),
-              ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  label: Text("Senha"),
-                ),
-              ),
+              )
             ],
           ),
-        )
+        ),
       ],
     );
   }
---
+++ lib/template/components/input_personalizado.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class InputPersonalizado extends StatelessWidget {
+  final bool? senha;
+  final String texto;
+  const InputPersonalizado({super.key, required this.texto, this.senha = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Center(
+        child: Container(
+          height: 50,
+          decoration: BoxDecoration(
+            color: const Color(0XFF8E8870),
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: TextField(
+            obscureText: senha!,
+            cursorColor: Colors.white,
+            decoration: InputDecoration(
+              labelText: texto,
+              labelStyle: const TextStyle(
+                color: Colors.white,
+              ),
+              border: InputBorder.none,
+              contentPadding: const EdgeInsets.only(left: 20, right: 20),
+            ),
+            style: const TextStyle(
+              color: Colors.white,
+              fontFamily: 'Raleway',
+              fontSize: 20,
+              fontVariations: [
+                FontVariation('wght', 400),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
